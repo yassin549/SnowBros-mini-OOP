cbd80a693015eb01c0243a1397d659efec932a4f
yassin549 <khoualdiyassin26@gmail.com>
2019-09-19 10:30:15 +0500

    Fixed collision bugs between snowballs and enemies

---
+++ Library/Framework/Interfaces/ICollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FreedomFodgeFrameWork.Interfaces
+{
+    public interface ICollision
+    {
+        bool BoundaryWallCollision(Game game);
+        bool PlayerEnemyCollision(Game game);
+        bool FireCollision(Game game,PictureBox pictureBox);
+    }
+}

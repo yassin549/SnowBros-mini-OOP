53e458d913426e9a585e39308c8858a53cf2eb55
yassin549 <khoualdiyassin26@gmail.com>
2019-10-03 09:12:40 +0500

    Added sound effects for player actions and enemy interactions

---
+++ UI/FreedomFodge/ImagePaths.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreedomFodge
+{
+    internal class ImagePaths
+    {
+       public static string MazeTile1 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\tile1.png";
+       public static string MazeTile2 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\tile2.png";
+
+        public static string LeftPlayerImage1 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\left1.png";
+        public static string LeftPlayerImage2 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\left2.png";
+
+        public static string RightPlayerImage1 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\right1.png";
+        public static string RightPlayerImage2 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\right2.png";
+
+        public static string UpPlayerImage = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\UpAndDown.png";
+        public static string DownPlayerImage = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\UpAndDown.png";
+
+        public static string LeftEnemy1Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy1L.png";
+        public static string RightEnemy1Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy1R.png";
+
+        public static string LeftEnemy2Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy2L.png";
+        public static string RightEnemy2Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy2R.png";
+
+        public static string LeftEnemy3Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy3L.png";
+        public static string RightEnemy3Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy3R.png";
+
+        public static string Enemy4Image = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\boss.png";
+        public static string PlayerFire = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\PBullet.png";
+        public static string EnemyFire1 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy1Bullet.png";
+        public static string EnemyFire2 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy2Bullet.png";
+        public static string EnemyFire3 = "C:\\Users\\Administrator\\Desktop\\OOP GAME\\sprites\\enemy3Bullet.gif";
+
+
+    }
+}
